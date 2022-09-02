6d0bb4c80af17f69ebac62fd088e97947f9be681
celinaramos <112406630+celinaramos@users.noreply.github.com>
2022-09-02 13:45:54 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString(texto1)
+})
+let texto1 = ""
+texto1 = ""
+let texto2 = ""
+let numero1 = 0
+let numero2 = 0
+basic.forever(function () {
+	
+})
