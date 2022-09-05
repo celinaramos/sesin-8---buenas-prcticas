11f14bb3c324e8b1a8929bdc42ce620d968f7197
celinaramos <112406630+celinaramos@users.noreply.github.com>
2022-09-05 07:34:44 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString(texto1)
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(numero)
+    numero += 1
 })
-let texto1 = ""
-texto1 = ""
-let texto2 = ""
-let numero1 = 0
-let numero2 = 0
+input.onButtonPressed(Button.AB, function () {
+    texto = "bye"
+})
+input.onButtonPressed(Button.B, function () {
+    numero += -1
+    basic.showNumber(numero)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("" + (texto))
+})
+let numero = 0
+let texto = ""
+texto = "hi"
+numero = 0
 basic.forever(function () {
 	
 })
